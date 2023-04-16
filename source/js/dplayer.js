6a3dcdf2e3a0b30043368b5a533254996a8fa99f
angelghost44 <55450289+angelghost44@users.noreply.github.com>
2023-04-16 22:14:35 +0800

    Update dplayer.js

--- source/js/dplayer.js
+++ source/js/dplayer.js
@@ -1,12 +1,1 @@
-<script>
-        let dock = document.getElementById("dp");
-        let du = dock.querySelector("video");
-        console.log(du.src);
-        const dp = new DPlayer({
-            container: dock,
-            video: {
-                url: du.src,
-                hotkey: true,
-            },
-        });
-    </script>
+
